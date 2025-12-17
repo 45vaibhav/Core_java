e11b2a5dc8c784918741b79ec677ccccbdc079e3
Vaibhav <vaibhavkapde445@gmail.com>
2025-12-17 23:55:35 +0530

    accurate arrange

--- Java-Assignment/Pattern_codes/p5.java
+++ Java-Assignment/Pattern_codes/p5.java
@@ -5,16 +5,15 @@
 		for(int j=1;j<=5;j++){
 		if(j<=5-i){
 		System.out.print(" ");
-			}
+		}
 		else{
-
-		System.out.print("*");					}
-						}
-
+		System.out.print("*");	
+		}
+	     }
 		System.out.println("");
-					}
-				}
-			}
+         }
+     }
+  }
 
 	
 	//o/p:   *
