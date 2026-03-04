55fec461029e6035ae17be12bb148e5b2b614453
Vaibhav <vaibhavkapde445@gmail.com>
2026-03-04 22:20:53 +0530

    int to String

---
+++ JavaPractice/String/p11.java
@@ -0,0 +1,14 @@
+class p11{
+    public static void main(String args[]){
+        int n=100;
+        String s=Integer.toString(n);
+        if(n==Integer.parseInt(s)){
+            System.out.println("well converted ");
+        }else{
+            System.out.println("not conveted");
+        }
+    }
+}
+
+
+//O/P :well converted 
